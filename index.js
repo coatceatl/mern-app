a4a62b3e26ec300fe55b11f7e5f84188de21ed51
Oksana Tkachenko <oksana@crystax.net>
2019-04-07 22:09:16 +0300

    refactored structure

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes.js');
+require('./services/passport.js');
+
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys.js');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
+authRoutes(app);
 
-  }
-));
-
-app.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 4000;
 
