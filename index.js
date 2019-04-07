68b0c464c30f7c757c32f772877534b5fa43930e
Oksana Tkachenko <oksana@crystax.net>
2019-04-07 20:41:18 +0300

    WIP, installed passport.js and added auth code

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
-app.get('/', (req, res) => {
-  res.send('Hi from server');
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  accessToken => {
+    console.log(accessToken);
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
 
 const PORT = process.env.PORT || 4000;
 
