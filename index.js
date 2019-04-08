2e9fa75b337b3f32b350942184c4c146ecbe2ab1
Oksana Tkachenko <oksana@crystax.net>
2019-04-08 11:56:41 +0300

    installed mongoose and created mongoose model class

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes.js');
+require ('./models/User');
 require('./services/passport.js');
 
+mongoose.connect('mongodb://localhost:27017/mern-app', { useNewUrlParser: true }) ;
 const app = express();
 
 authRoutes(app);
 
-
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, ()=> {
   console.log('server started');
 });
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -8,9 +11,6 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, cb) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
-
+    new User({ googleId: profile.id }).save();
   }
 ));
